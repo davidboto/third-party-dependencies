f46b51efad5a065d9174bd18b51c2fed0c2ba1a6
David Aragão <david.aragao@sti.ufc.br>
2020-04-16 02:19:07 -0300

    Install a commander.

--- index.js
+++ index.js
@@ -1,34 +1,57 @@
-const pathModules = "./node_modules/";
 const fs = require('fs');
+const { program } = require('commander');
+
+program.on('--help', () => {
+  console.log('');
+  console.log('Example call:');
+  console.log('  $ index.js --help');
+});
+
+program
+  .requiredOption('-s, --node-module <path>', 'defines the path to the node_module folder')
+  .option('-d, --destination <path>', 'defines the file to output the result')
+
+program.parse(process.argv);
 
 let packageList = [];
 
 try {
 
+  const pathModules = program.nodeModule;
   const modules = fs.readdirSync(pathModules);
 
   modules.forEach((folder) => {
 
     let license;
     let packageContent;
-  
-    const folderFiles = fs.readdirSync(pathModules + folder + "/");
-  
-    if (fs.existsSync(pathModules + folder + "/LICENSE")) {
-      license = fs.readFileSync(pathModules + folder + "/LICENSE", "utf8")
-    }
-    
-    if (fs.existsSync(pathModules + folder + "/package.json")) {
-      packageContent = fs.readFileSync(pathModules + folder + "/package.json", "utf8")
-    }
+    let filteredPackage;
 
-    if(packageContent !== undefined){
-      packageContent = JSON.parse(packageContent)
-    } else {
-      packageContent = {}
+    if (fs.lstatSync(pathModules + folder).isDirectory()) {
+
+      if (fs.existsSync(pathModules + folder + "/LICENSE")) {
+        license = fs.readFileSync(pathModules + folder + "/LICENSE", "utf8")
+      }
+
+      if (fs.existsSync(pathModules + folder + "/package.json")) {
+        packageContent = fs.readFileSync(pathModules + folder + "/package.json", "utf8")
+      }
+
+      if (packageContent !== undefined) {
+        packageContent = JSON.parse(packageContent)
+        filteredPackage = {
+          "name": packageContent.name,
+          "version": packageContent.version,
+          "description": packageContent.description,
+          "author": packageContent.author,
+        };
+      } else {
+
+        filteredPackage = {}
+      }
+
+      packageList.push({ folder, license, filteredPackage });
+
     }
-  
-    packageList.push({ folder, folderFiles, license, packageContent });
 
   });
 
@@ -36,7 +59,9 @@ try {
   console.log(err);
 }
 
-fs.writeFile("dependencies.json", JSON.stringify(packageList), "utf8", data => {
+let resultFile = program.destination || "depencencies.json"
+
+fs.writeFile(resultFile, JSON.stringify(packageList), "utf8", data => {
   console.log("ok")
 });
 
